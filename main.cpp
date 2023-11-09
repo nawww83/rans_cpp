31005b0c0014fd0e8c303347b5d763a09ed211c5
Anatoly V Novikov <nawww83@gmail.com>
2023-11-09 15:42:53 +0300

    Add median performance calculation

--- main.cpp
+++ main.cpp
@@ -41,22 +41,16 @@ int main() {
     std::srand(std::time(0));
 
     double dt = 0;
-    double current_perf;
-    auto disp_perf = [&dt, &current_perf](int n, bool display=true) {
-        current_perf = (1.e3 * n / dt);
-        if (display) {
-            std::cout << " performance: " << current_perf << " MB/s" << std::endl;
-        }
+    auto calc_perf = [&dt](int n) {
+        return (1.e3 * n / dt); // MB/s
     };
 
     rans::Rans rans;
-    // cnt::Counter<65536> c;
     int iters = 0;
     constexpr int N_max = 8*1024*1024;
     const int N_performance_is_mearured = N_max / 4;
     while (true) {
         size_t N = (((size_t)std::rand()) % N_max) + 1;
-        // size_t N = 32;
         std::vector<u8> v(N);
         std::vector<u8> output(rans.required_bytes(N));
 
@@ -67,17 +61,12 @@ int main() {
 
         std::vector<u8> v_decoded(N);
         constexpr int Q = 128;
-        std::vector<double> perfomances(Q);
         bool inv_f = std::rand() % 2;
 
         cout << " Test is started, inversion flag: " << inv_f << ", please, wait..." << endl;
         std::vector<double> CRs {};
-        double min_decomp_perf = 1.e18;
-        double max_decomp_perf = 0;
-        double min_comp_perf = 1.e18;
-        double max_comp_perf = 0;
-        double min_CR = 1.e18;
-        double max_CR = 0;
+        std::vector<double> comp_perfs {};
+        std::vector<double> decomp_perfs {};
         for (int q=0; q<=Q; ++q) {
             const double par = double(q) / double(Q);
             GeometricDistribution<int> r(par);
@@ -85,65 +74,41 @@ int main() {
             int out_size;
             timer.reset();
             rans.encode(v.data(), N, output.data(), out_size);
-            // auto f { c.count(v.data(), N) };
             dt = timer.elapsed_ns();
-            // cnt::u64 sum_f = 0;
-            // for (auto el : f) {
-                // sum_f += el;
-            // }
-            // assert(sum_f == c.get_L());
-            // cout << " geometric distr. p: " << par << ", sum of frequencies: " << sum_f << ", ";
             const double CR = double(N) / double(out_size);
             CRs.push_back(CR);
-            min_CR = std::min(min_CR, CR);
-            max_CR = std::max(max_CR, CR);
-            // cout << " geom. distr. p: " << par << ", compressed size: " << out_size << ", bytes. CR: " << CR << ", ";
             if (measure_perf) {
-                // cout << "Geometric distribution p: " << par << ", CR: " << CR << endl;
                 // cout << "Compression: ";
-                disp_perf(N, false);
-                min_comp_perf = std::min(min_comp_perf, current_perf);
-                max_comp_perf = std::max(max_comp_perf, current_perf);
+                comp_perfs.push_back(calc_perf(N));
             }
             assert(CR >= 1.00000);
             //
             timer.reset();
             rans.decode(output.data(), out_size, v_decoded.data(), N);
             dt = timer.elapsed_ns();
-            // cout << "  decompressed size: " << v_decoded.size() << ", bytes" << ", ";
             const bool is_equal = std::equal(v.begin(), v.end(), v_decoded.begin());
-            // if (! is_equal) {
-            //     std::cout << "Original vector: ";
-            //     for (const auto& el : v) {
-            //         std::cout << int(el) << ", ";
-            //     }
-            //     std::cout << std::endl;
-            //     std::cout << "Decompressed vector: ";
-            //     for (const auto& el : v_decoded) {
-            //         std::cout << int(el) << ", ";
-            //     }
-            //     std::cout << std::endl;
-            // }
             assert( is_equal );
             if (measure_perf) {
                 // cout << " Decompression: ";
-                disp_perf(N, false);
-                min_decomp_perf = std::min(min_decomp_perf, current_perf);
-                max_decomp_perf = std::max(max_decomp_perf, current_perf);
+                decomp_perfs.push_back(calc_perf(N));
             }
         }
+        // Get some statistics
         std::sort(CRs.begin(), CRs.end());
-        const double CR_median = CRs[CRs.size()/2];
+        std::sort(comp_perfs.begin(), comp_perfs.end());
+        std::sort(decomp_perfs.begin(), decomp_perfs.end());
+        const int NN = CRs.size();
+        const int NNc = comp_perfs.size();
+        assert(comp_perfs.size() == decomp_perfs.size());
         cout << endl;
         cout << "Input length: " << N << " bytes" << endl;
-        cout << "Median CR: " << CR_median << endl;
-        cout << "Min CR: " << min_CR << endl;
-        cout << "Max CR: " << max_CR << endl;
+        cout << "Compression Ratio, CR:" << endl;
+        cout << " Min: " << CRs[0] << ", Max: " << CRs[NN-1] << ", Median: " << CRs[NN / 2] << endl;
         if (measure_perf) {
-            cout << "Min decompression performance: " << min_decomp_perf << " MB/s" << endl;
-            cout << "Max decompression performance: " << max_decomp_perf << " MB/s" << endl;
-            cout << "Min compression performance: " << min_comp_perf << " MB/s" << endl;
-            cout << "Max compression performance: " << max_comp_perf << " MB/s" << endl;
+            cout << "Decompression performance, MB/s:" << endl;
+            cout << " Min: " << decomp_perfs[0] << ", Max: " << decomp_perfs[NNc-1] << ", Median: " << decomp_perfs[NNc / 2] << endl;
+            cout << "Compression performance, MB/s:" << endl;
+            cout << " Min: " << comp_perfs[0] << ", Max: " << comp_perfs[NNc-1] << ", Median: " << comp_perfs[NNc / 2] << endl;
         } else {
             cout << "Performance measurement is skipped due to small input size: " << N << " that is less than: " << N_performance_is_mearured << endl;
         }
